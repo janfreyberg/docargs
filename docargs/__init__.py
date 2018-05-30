1eaeeb2dae95d7367af226fa8361ab4a62224cd6
Jan Freyberg <jan.freyberg@gmail.com>
2018-05-30 23:33:00 +0100

    Updated the package to raise an error if parameters are not documented.

--- docargs/__init__.py
+++ docargs/__init__.py
@@ -5,41 +5,58 @@ import click
 
 
 def check_docced_args(module):
+    failures = 0
     for name, obj in inspect.getmembers(module):
-        if (inspect.isclass(obj)
-                and obj.__module__ == module.__name__
-                and name[0] != '_'):
+        if (
+            inspect.isclass(obj)
+            and obj.__module__ == module.__name__
+            and name[0] != "_"
+        ):
             signature_args = [
-                arg for arg in inspect.getfullargspec(obj.__init__).args
-                if arg != 'self'
+                arg
+                for arg in inspect.getfullargspec(obj.__init__).args
+                if arg != "self"
             ]
             docced_args = [
-                arg[0] for arg in
-                NumpyDocString(inspect.getdoc(obj))['Parameters']
+                arg[0]
+                for arg in NumpyDocString(inspect.getdoc(obj))["Parameters"]
             ]
             if set(signature_args) != set(docced_args):
-                print('class: ', name)
-                print('parameters: ', signature_args)
-                print('documented: ', docced_args)
-        elif (inspect.isfunction(obj)
-              and obj.__module__ == module.__name__
-              and name[0] != '_'):
-            print('function: ', name)
+                print(
+                    "class: {}; not documented: {}".format(
+                        name, ', '.join(set(signature_args) - set(docced_args))
+                    )
+                )
+                failures += 1
+        elif (
+            inspect.isfunction(obj)
+            and obj.__module__ == module.__name__
+            and name[0] != "_"
+        ):
             signature_args = inspect.getfullargspec(obj).args
             docced_args = [
-                arg[0] for arg in
-                NumpyDocString(inspect.getdoc(obj))['Parameters']
+                arg[0]
+                for arg in NumpyDocString(inspect.getdoc(obj))["Parameters"]
             ]
             if set(signature_args) != set(docced_args):
-                print('class: ', name)
-                print('parameters: ', signature_args)
-                print('documented: ', docced_args)
+                print(
+                    "function: {}; not documented: {}".format(
+                        name, ', '.join(set(signature_args) - set(docced_args))
+                    )
+                )
+                failures += 1
         elif inspect.ismodule(obj) and obj.__package__ == module.__name__:
-            check_docced_args(obj)
+            failures += check_docced_args(obj)
+
+    return failures
 
 
 @click.command()
-@click.argument('modulename')
-def cli(modulename=''):
+@click.argument("modulename")
+def cli(modulename=""):
     module = importlib.import_module(modulename)
-    check_docced_args(module)
+    failures = check_docced_args(module)
+    if failures > 0:
+        raise ValueError(
+            "Undocumented parameters in non-private function or classes!"
+        )
