3f7567b30bbe311218720e65461ab69373b14cd3
Jan Freyberg <janfreyberg@janfreyberg-macbookair.roam.corp.google.com>
2020-05-03 13:22:37 +0100

    Fix install in github action

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     packages=["docargs"],
     keywords=["linting"],
     install_requires=requirements,
-    extras_require={"tests": test_requirements},
+    extras_require={"tests": test_requirements, "test": test_requirements},
     entry_points={
         "console_scripts": ["docargs = docargs.cli:cli"],
         "flake8.extension": ["D00 = docargs.flake8:DocargsChecker"],
